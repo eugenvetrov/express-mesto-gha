ff1410f13d154ba1754a4e6c47a1a1990234a1a1
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-05-22 12:43:18 +0500

    Make basic app, db, modelsm route

---
+++ controllers/cards.js
@@ -0,0 +1,24 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const createCard = (req, res) => {
+  const owner = req.user._id;
+  const { name, link } = req.body;
+
+  Card.create({ name, link, owner })
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const deleteCardById = (req, res) => {
+  Card.findByIdAndRemove(req.params.id)
+    .then((card) => res.send({ data: card }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = { getCards, createCard, deleteCardById };
