73d575d2167232f33aaeb04d401b0851281fd1f2
Eugene Vetrov <evg.vetrow@yandex.ru>
2022-06-07 20:50:54 +0500

    ordinary fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,11 +3,10 @@ const Unauthorized = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer') || !req.headers.cookie.startsWith('token')) {
+  if (!authorization) {
     next(new Unauthorized('Необходима авторизация'));
   } else {
-    let token;
-    if (authorization) { token = authorization.replace('Bearer ', ''); } else if (req.headers.cookie.startsWith('token')) { token = authorization.replace('token=', ''); }
+    const token = authorization.replace('Bearer ', '');
     let payload;
     try {
       payload = jwt.verify(token, 'some-secret-key');
